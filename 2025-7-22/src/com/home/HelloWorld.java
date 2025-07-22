f0dd1647b009a977cc73e6eaf6ef933bffdbfa22
lening0 <lixiangning88@gmail.com>
2025-07-22 19:12:11 +0800

    Add files via upload

---
+++ 2025-7-22/src/com/home/HelloWorld.java
@@ -0,0 +1,173 @@
+package com.home;
+
+//public class HelloWorld {
+//    //叫做main方法，表示程序的主入口
+//    public static void main(String[] args) {
+//        //输出HelloWorld
+//        System.out.println("Hello World");
+//    }
+//}
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+////        System.out.println(666);//整数
+////        System.out.println(-2);//负数
+////        System.out.println(0.234);//小数
+////        System.out.println("lxn");//字符串
+////        System.out.println('男');//字符
+////        System.out.println(true);//布尔
+////        System.out.println(false);//布尔
+//        System.out.println("name"+'\t'+"age");
+//        System.out.println("lxn "+'\t'+"19");
+//    }
+//}
+
+//public class HelloWorld {
+//    public static void main(String[] args){
+//        //定义变量
+//        int a = 2;//整数
+//        double b = 0.2;//小数
+//        a = 3; //重新定义
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(a + b);//a与b和
+//    }
+//}
+public class HelloWorld {
+    public static void main(String[] args) {
+        int count = 0;
+        count++;
+        count+=2;
+        count--;
+        count+=2;
+        count--;
+        count--;
+        count++;
+        System.out.println(count);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
